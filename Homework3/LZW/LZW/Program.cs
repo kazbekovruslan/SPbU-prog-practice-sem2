4c264521f59e351db4d96040c90c251f6c7613f3
kazbekovruslan <chartusss@yandex.ru>
2023-03-28 16:08:34 +0300

    make args in main for run with args from terminal

--- Homework3/LZW/LZW/Program.cs
+++ Homework3/LZW/LZW/Program.cs
@@ -4,11 +4,56 @@ using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        //reading bytes - compressing - writing compressed bytes
-        var pathToFile = "WarAndPeace.txt";
-        var input = File.ReadAllBytes(pathToFile);
-        File.WriteAllBytes(pathToFile + ".zipped", LZWEncoder.Encode(input));
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Incorrect amount of arguments. Enter 'dotnet run [file name] [--c/--u]'.");
+        }
+        else
+        {
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("File not found!");
+            }
+            else
+            {
+                switch (args[1])
+                {
+                    case "--c":
+                        var pathToFile = args[0];
+                        var input = File.ReadAllBytes(pathToFile);
+                        var output = LZWEncoder.Encode(input);
+                        File.WriteAllBytes(pathToFile + ".zipped", output);
+                        Console.WriteLine("Compressing done!");
+                        Console.WriteLine($"Compression ratio: {(double)input.Length / output.Length}");
+                        break;
+                    case "--u":
+                        var pathToZippedFile = args[0];
+                        input = File.ReadAllBytes(pathToZippedFile);
+                        File.WriteAllBytes(pathToZippedFile.Replace(".zipped", ""), LZWDecoder.Decode(input));
+                        Console.WriteLine("Decompressing done!");
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect function! There are only -c and -u!");
+                        break;
+                }
+            }
+        }
+
+        // var pathToFile = "testFiles/more.mp4";
+        // var input = File.ReadAllBytes(pathToFile);
+        // var output = LZWEncoder.Encode(input);
+        // File.WriteAllBytes(pathToFile + ".zipped", output);
+        // Console.WriteLine("Compressing done!");
+        // Console.WriteLine($"Compression ratio: {(double)input.Length / output.Length}");
+
+        // var pathToZippedFile = "testFiles/more.mp4.zipped";
+        // input = File.ReadAllBytes(pathToZippedFile);
+        // File.WriteAllBytes(pathToZippedFile.Replace(".zipped", ""), LZWDecoder.Decode(input));
+        // Console.WriteLine("Decompressing done!");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
     }
 }
