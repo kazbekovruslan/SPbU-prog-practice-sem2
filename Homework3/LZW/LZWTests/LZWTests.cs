f5e61570ccbc1b5754bf46c26a605e5036786dfa
kazbekovruslan <chartusss@yandex.ru>
2023-03-28 17:49:09 +0300

    add unit tests

--- Homework3/LZW/LZWTests/LZWTests.cs
+++ Homework3/LZW/LZWTests/LZWTests.cs
@@ -1,15 +1,52 @@
 namespace LZWTests;
 
-public class Tests
+using LZW;
+
+public class LZWTests
 {
-    [SetUp]
-    public void Setup()
+    private static IEnumerable<TestCaseData> TestFiles()
     {
+        yield return new TestCaseData("../../../testFiles/more.mp4");
+        yield return new TestCaseData("../../../testFiles/v.mp4");
+        yield return new TestCaseData("../../../testFiles/WarAndPeace.txt");
+        yield return new TestCaseData("../../../testFiles/minesweeper.exe");
     }
 
     [Test]
-    public void Test1()
+    public void EncoderWithEmptyInputArrayOfBytesShouldThrowException()
     {
-        Assert.Pass();
+        var input = new byte[0];
+        Assert.Throws<ArgumentException>(() => LZWEncoder.Encode(input));
+    }
+
+    [Test]
+    public void EncoderWithNullInputArrayOfBytesShouldThrowException()
+    {
+        byte[]? input = null;
+        Assert.Throws<ArgumentNullException>(() => LZWEncoder.Encode(input));
+    }
+
+    [Test]
+    public void DecoderWithEmptyInputArrayOfBytesShouldThrowException()
+    {
+        var input = new byte[0];
+        Assert.Throws<ArgumentException>(() => LZWDecoder.Decode(input));
+    }
+
+    [Test]
+    public void DecoderWithNullInputArrayOfBytesShouldThrowException()
+    {
+        byte[]? input = null;
+        Assert.Throws<ArgumentNullException>(() => LZWDecoder.Decode(input));
+    }
+
+    [TestCaseSource(nameof(TestFiles))]
+    public void EncoderAndDecoderWithRightInputShouldOutputOriginalFile(string pathToFile)
+    {
+        var input = File.ReadAllBytes(pathToFile);
+        var encoderOutput = LZWEncoder.Encode(input);
+        var decoderOutput = LZWDecoder.Decode(encoderOutput);
+
+        Assert.That(input, Is.EqualTo(decoderOutput));
     }
 }
