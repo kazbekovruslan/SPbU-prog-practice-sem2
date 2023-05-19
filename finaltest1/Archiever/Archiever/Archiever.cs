0d416f4dd1a88af04b3394b295bdfeb16c83c165
kazbekovruslan <chartusss@yandex.ru>
2023-05-19 15:16:27 +0300

    complete task1 - archiever - from finaltest

---
+++ finaltest1/Archiever/Archiever/Archiever.cs
@@ -0,0 +1,110 @@
+namespace Archiever;
+
+public class Archiever
+{
+    private const int maxAvailableValueInByte = 255;
+
+    public (byte[], float compressRatio) Compress(byte[]? inputByteArray)
+    {
+        if (inputByteArray == null)
+        {
+            throw new ArgumentNullException("Byte array can't be null!");
+        }
+
+        if (inputByteArray.Length == 0)
+        {
+            throw new ArgumentException("Byte array can't be empty!");
+        }
+
+        var output = new List<byte>();
+        byte lastByte = inputByteArray[0];
+        int duplicateByteCounter = 0;
+
+        for (int i = 0; i < inputByteArray.Length; ++i)
+        {
+            if (inputByteArray[i] != lastByte)
+            {
+                while (duplicateByteCounter > maxAvailableValueInByte)
+                {
+                    output.Add((byte)maxAvailableValueInByte);
+                    output.Add(lastByte);
+                }
+
+                output.Add((byte)duplicateByteCounter);
+                output.Add(lastByte);
+
+                lastByte = inputByteArray[i];
+                duplicateByteCounter = 1;
+            }
+            else
+            {
+                ++duplicateByteCounter;
+            }
+        }
+
+        // remaining
+        while (duplicateByteCounter > maxAvailableValueInByte)
+        {
+            output.Add((byte)maxAvailableValueInByte);
+            output.Add(lastByte);
+            duplicateByteCounter -= maxAvailableValueInByte;
+        }
+        if (duplicateByteCounter != 0)
+        {
+            output.Add((byte)duplicateByteCounter);
+            output.Add(lastByte);
+        }
+
+        var outputArrayOfBytes = output.ToArray();
+        var compressRatio = (float)inputByteArray.Length / (float)outputArrayOfBytes.Length;
+
+        return (outputArrayOfBytes, compressRatio);
+    }
+
+
+    public byte[] Decompress(byte[]? inputByteArray)
+    {
+        if (inputByteArray == null)
+        {
+            throw new ArgumentNullException("Byte array can't be null!");
+        }
+
+        if (inputByteArray.Length == 0)
+        {
+            throw new ArgumentException("Byte array can't be empty!");
+        }
+
+        if (inputByteArray.Length % 2 != 0)
+        {
+            throw new ArgumentException("Incorrect input byte array!");
+        }
+
+        byte currentByte = 0;
+        var amountOfCurrentByte = 0;
+        byte previousByte = 0;
+        var amountOfPreviousByte = 0;
+
+        List<byte> output = new();
+
+        for (int i = 0; i < inputByteArray.Length; i = i + 2)
+        {
+            amountOfCurrentByte = inputByteArray[i];
+            currentByte = inputByteArray[i + 1];
+
+            if (amountOfCurrentByte != 255)
+            {
+                for (int j = 0; j < amountOfCurrentByte + amountOfPreviousByte; ++j)
+                {
+                    output.Add(currentByte);
+                }
+            }
+            else
+            {
+                previousByte = currentByte;
+                amountOfPreviousByte += amountOfCurrentByte;
+            }
+        }
+
+        return output.ToArray();
+    }
+}
